f59fc6f529f822d3b2faad7225b55d80818a5eb7
HazarAlpay <hazar.alpay@ug.bilkent.edu.tr>
2025-03-15 17:44:45 +0300

    Third iteration of the Harmonia. Login/Sign Up, Home, Search, Review, User Profile, and Authentication Settings mostly completed. Some bugs and issues have been resolved.

--- app/constants/apiConstants.js
+++ app/constants/apiConstants.js
@@ -1,5 +1,26 @@
 // Buradaki bilgiler demo süreci için. Üretim ortamında backend'e aktarılmalıdır.
+const IP_ADDRESS = "http://192.168.0.27"; //TODO: buranın local host ile neden çalışmadığını anlamaya çalışın
+
+//HARMONIA APP
+
+//DEMO APP
+
 export const REDIRECT_URI = "exp://172.20.10.2:8081"; // Geliştirme sırasında kullanılması gereken URI, üretimde değiştirilmelidir
+
 export const AUTH_URL = "https://accounts.spotify.com/authorize";
 export const TOKEN_URL = "https://accounts.spotify.com/api/token";
 export const SCOPES = "user-library-read playlist-read-private user-top-read";
+
+// Backend Services
+export const BACKEND_AMAZON_SERVICES_URL = `${IP_ADDRESS}:8000`;
+export const BACKEND_API_GATEWAY_URL = `${IP_ADDRESS}:8765`;
+export const BACKEND_COMMENT_URL = `${IP_ADDRESS}:6525`;
+export const BACKEND_CREDENTIALS_URL = `${IP_ADDRESS}:1000`;
+export const BACKEND_EMAIL_SENDER_URL = `${IP_ADDRESS}:1500`;
+export const BACKEND_FAVORITE_URL = `${IP_ADDRESS}:6505`;
+export const BACKEND_PROFILE_API_URL = `${IP_ADDRESS}:2000`;
+export const BACKEND_PROFILE_PICTURE_DOWNLOADER_URL = `${IP_ADDRESS}:2121`;
+export const BACKEND_REVIEW_URL = `${IP_ADDRESS}:6605`;
+export const BACKEND_REVIEW_LIKE_URL = `${IP_ADDRESS}:2123`;
+export const BACKEND_SEARCH_PROFILE_URL = `${IP_ADDRESS}:6500`;
+export const BACKEND_USER_FOLLOW_URL = `${IP_ADDRESS}:6421`;
